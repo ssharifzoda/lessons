e61edfe9dcb1b263641f464c8b27f1e2e9e6aa63
SharifzodaS <sharifzoda.sino@gmail.com>
2025-10-13 17:13:13 +0500

    lesson 5

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var a, b int
 
-	fmt.Print("Введите первое число: ")
-	fmt.Scan(&a)
-
-	fmt.Print("Введите второе число: ")
-	fmt.Scan(&b)
-
-	fmt.Println(a, b)
 }
